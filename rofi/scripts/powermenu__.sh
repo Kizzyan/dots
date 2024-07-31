6728b84fe6f77e19f20e20b3fefa3ae002933653
Kizzyan <yan.leitao26@gmail.com>
2024-07-30 23:55:11 -0300

    Alterações rofi e kitty

--- rofi/scripts/powermenu__.sh
+++ rofi/scripts/powermenu__.sh
@@ -3,8 +3,8 @@
 chosen=$(printf "󰐥 Power Off\n󰜉 Restart\n󰈆 Exit" | rofi -dmenu -i -theme-str '@import ".config/rofi/scripts/powermenu.rasi"')
 
 case "$chosen" in 
-  " 󰐥 ") systemctl poweroff ;;
-  " 󰜉 ") systemtcl reboot ;;
-  " 󰈆 ") i3 exit ;;
+  "󰐥 Power Off") systemctl poweroff ;;
+  "󰜉 Restart") systemtcl reboot ;;
+  "󰈆 Exit") i3 exit ;;
   *) exit 1 ;;
 esac
