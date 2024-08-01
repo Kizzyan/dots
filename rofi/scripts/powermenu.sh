3281d1c5d6ade22b6fcc28ceb9d02da977f1d77c
Kizzyan <yan.leitao26@gmail.com>
2024-07-31 21:44:53 -0300

    Alterações Rofi e Polybar

--- rofi/scripts/powermenu.sh
+++ rofi/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-chosen=$(printf "󰐥 Power Off\n󰜉 Restart\n󰈆 Exit" | rofi -dmenu -i -theme-str '@import ".config/rofi/scripts/powermenu.rasi"')
+chosen=$(printf " 󰐥 \n 󰜉 \n 󰈆 " | rofi -dmenu -i -theme-str '@import ".config/rofi/scripts/powermenu.rasi"')
 
 case "$chosen" in 
   " 󰐥 ") systemctl poweroff ;;
