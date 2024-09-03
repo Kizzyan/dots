c2fd4c2a567a0aa776c6c9fefb02f5f799dce1d3
Kizzyan <yan.leitao26@gmail.com>
2024-09-03 20:27:29 -0300

    Melhoria no módulo de pesquisa do rofi e alterações visuais nos demais módulos

--- rofi/scripts/websearch.sh
+++ rofi/scripts/websearch.sh
@@ -5,21 +5,22 @@
 options="g\ny\nw"
 
 # Use Rofi to select the search engine
-selected=$(echo -e "$options" | rofi -dmenu -i -p " " -theme-str '@import ".config/rofi/scripts/websearch.rasi"')
+# selected=$(echo -e "$options" | rofi -dmenu -i -p " " -theme-str '@import ".config/rofi/scripts/websearch2.rasi"')
+selected=$(printf "󰊭\n󰗃\n󰖬" | rofi -dmenu -i -theme-str '@import ".config/rofi/scripts/websearch2.rasi"')
 
 # Set the search URL based on the selected search engine
 case "$selected" in
-    g)
+    "󰊭")
         search_url="http://www.google.com/search?q="
-        icon=" "
+        icon="  "
         ;;
-    y)
+    "󰗃")
         search_url="https://www.youtube.com/results?search_query="
-        icon=" "
+        icon="  "
         ;;
-    w)
+    "󰖬")
         search_url="https://en.wikipedia.org/wiki/"
-        icon=" "
+        icon="  "
         ;;
     *)
         exit 1
@@ -27,7 +28,7 @@ case "$selected" in
 esac
 
 # Prompt the user for the search query
-query=$(rofi -dmenu -i -p "$icon " -theme-str '@import ".config/rofi/scripts/websearch.rasi"')
+query=$(rofi -dmenu -i -p "$icon " -theme-str '@import ".config/rofi/scripts/websearch1.rasi"')
 
 # Open the selected search engine with the query
 if [ -n "$query" ]; then
